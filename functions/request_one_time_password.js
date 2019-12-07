65faf80115310a746bf1f1857f8bf6418073d596
2imad <imadyoussoufi@gmail.com>
2019-12-07 14:48:18 +0100

    adjust process.env

--- functions/request_one_time_password.js
+++ functions/request_one_time_password.js
@@ -1,8 +1,10 @@
 const helpers = require("./helpers");
 const admin = require("firebase-admin");
 const twilio = require("./twilio");
+const dotenv = require("dotenv");
 const PHONE = helpers.phone;
 const sanitize = helpers.sanitize;
+dotenv.config();
 
 module.exports = function(req, res) {
   if (!req.body.phone) {
