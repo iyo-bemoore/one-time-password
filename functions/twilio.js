d6da5155b61022eb9b4e8fa6fd318e5c5cfb4612
Imad Youssoufi <imadyoussoufi@gmail.com>
2019-12-07 13:54:40 +0100

    rem twiliio

--- functions/twilio.js
+++
@@ -1,6 +0,0 @@
-const twilio = require("twilio");
-
-const accountSid = "AC48a384b01165bad50930d5322da64b49";
-const authToken = "393948145a1146e5344f737e64e64983";
-
-module.exports = new twilio.Twilio(accountSid, authToken);
